42b21e10df6eb4836ae327c3d636231acd3eb5b0
g18m0999 <g18m0999@HUG131>
2020-02-20 16:45:30 +0200

    Added all files include .sln and test

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,24 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter input: ");
+            var ans = Console.ReadLine();
+            Prefix(ans);
+
+        }
+        static void Prefix(string Str)
+        {
+            var count = Str.Length;
+            char[] spaces = new char[] {' ', '\r', '\n' };
+            var wordCount = Str.Split(spaces,StringSplitOptions.RemoveEmptyEntries).Length; 
+            
+            Console.WriteLine(count + "," + wordCount + ":" + Str);
+            Console.ReadKey();
+
         }
     }
 }
